2096c910d6358aa41e42023d028c280369912044
Arthur Chin <77198012+AVCTY@users.noreply.github.com>
2022-11-13 08:59:51 +0000

    updated main page and added new chart script

--- scripts/chart4.js
+++ scripts/chart4.js
@@ -1,7 +1,17 @@
 // plot the data with Chart.js
+var chart = null;
+d3.csv('datasets/lunch_dataset/math_stats.csv').then(makeMathChart);
+document.getElementById("changingChartTitle").innerHTML = "Math Exam Pass Rate Based On Lunch Type";
+
+// create chart for Math results
 function makeMathChart(myData) {
     let delayed;
 
+    if (chart !== null) {
+        chart.destroy();
+    }
+
+    document.getElementById("changingChartTitle").innerHTML = "Math Exam Pass Rate Based On Lunch Type";
     // data for test preparation course status
     var lunchType = myData.map(function (d) {
         return d.lunch_type;
@@ -15,7 +25,7 @@ function makeMathChart(myData) {
 
 
     // create new chart
-    var chart = new Chart('changingCharts', {
+    chart = new Chart('changingCharts', {
         type: 'bar',
         data: {
             labels: lunchType,
@@ -57,7 +67,7 @@ function makeMathChart(myData) {
                     }
                     return delay;
                 },
-                duration: 500,
+                duration: 800,
                 easing: "easeOutQuad",
             },
             tooltips: {
@@ -68,15 +78,166 @@ function makeMathChart(myData) {
 }
 
 
+// create chart for Reading results
+function makeReadingChart(myData) {
+    let delayed;
+
+    if (chart !== null) {
+        chart.destroy();
+    }
+
+    document.getElementById("changingChartTitle").innerHTML = "Reading Exam Pass Rate Based On Lunch Type";
+    // data for test preparation course status
+    var lunchType = myData.map(function (d) {
+        return d.lunch_type;
+    });
+    var passRate = myData.map(function (d) {
+        return +d.pass;
+    });
+    var failRate = myData.map(function (d) {
+        return +d.fail;
+    });
+
+
+    // create new chart
+    chart = new Chart('changingCharts', {
+        type: 'bar',
+        data: {
+            labels: lunchType,
+            datasets: [
+                {
+                    label: "Pass",
+                    data: passRate,
+                    backgroundColor: 'rgba(0, 179, 60, 0.5)',
+                    hoverBackgroundColor: 'rgba(0, 179, 60, 1)',
+                    borderColor: 'rgba(0, 179, 60, 1)',
+                    borderWidth: 2,
+                    barPercentage: 0.6,
+                    categoryPercentage: 0.6,
+                },
+                {
+                    label: "Fail",
+                    data: failRate,
+                    backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                    hoverBackgroundColor: 'rgba(255, 0, 0, 1)',
+                    borderColor: 'rgba(255, 0, 0, 1)',
+                    borderWidth: 1,
+                    barPercentage: 0.6,
+                    categoryPercentage: 0.6,
+                    borderDash: [2, 6],
+                    borderDashOffset: 0.5,
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            animation: {
+                onComplete: () => {
+                    delayed = true;
+                },
+                delay: (context) => {
+                    let delay = 0;
+                    if (context.type === "data" && context.mode === "default" && !delayed) {
+                        delay = context.dataIndex * 400 + context.datasetIndex * 200;
+                    }
+                    return delay;
+                },
+                duration: 800,
+                easing: "easeOutQuad",
+            },
+            tooltips: {
+                titleFontSize: 30,
+            },
+        }
+    });
+}
+
+
+// create chart for Writing results
+function makeWritingChart(myData) {
+    let delayed;
+
+    if (chart !== null) {
+        chart.destroy();
+    }
+
+    document.getElementById("changingChartTitle").innerHTML = "Writing Exam Pass Rate Based On Lunch Type";
+    // data for test preparation course status
+    var lunchType = myData.map(function (d) {
+        return d.lunch_type;
+    });
+    var passRate = myData.map(function (d) {
+        return +d.pass;
+    });
+    var failRate = myData.map(function (d) {
+        return +d.fail;
+    });
+
+
+    // create new chart
+    chart = new Chart('changingCharts', {
+        type: 'bar',
+        data: {
+            labels: lunchType,
+            datasets: [
+                {
+                    label: "Pass",
+                    data: passRate,
+                    backgroundColor: 'rgba(0, 179, 60, 0.5)',
+                    hoverBackgroundColor: 'rgba(0, 179, 60, 1)',
+                    borderColor: 'rgba(0, 179, 60, 1)',
+                    borderWidth: 2,
+                    barPercentage: 0.6,
+                    categoryPercentage: 0.6,
+                },
+                {
+                    label: "Fail",
+                    data: failRate,
+                    backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                    hoverBackgroundColor: 'rgba(255, 0, 0, 1)',
+                    borderColor: 'rgba(255, 0, 0, 1)',
+                    borderWidth: 1,
+                    barPercentage: 0.6,
+                    categoryPercentage: 0.6,
+                    borderDash: [2, 6],
+                    borderDashOffset: 0.5,
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            animation: {
+                onComplete: () => {
+                    delayed = true;
+                },
+                delay: (context) => {
+                    let delay = 0;
+                    if (context.type === "data" && context.mode === "default" && !delayed) {
+                        delay = context.dataIndex * 400 + context.datasetIndex * 200;
+                    }
+                    return delay;
+                },
+                duration: 800,
+                easing: "easeOutQuad",
+            },
+            tooltips: {
+                titleFontSize: 30,
+            },
+        }
+    });
+}
+
+
+// function to switch between charts when selecting an option from dropdown menu
 function changeCharts() {
     var myVal = document.getElementById("chart4choices").value;
     if (myVal === "math") {
         d3.csv('datasets/lunch_dataset/math_stats.csv').then(makeMathChart);
     }
     else if (myVal === "reading") {
-        d3.csv('datasets/lunch_dataset/math_stats.csv').then(makeMathChart);
+        d3.csv('datasets/lunch_dataset/reading_stats.csv').then(makeReadingChart);
     }
     else if (myVal === "writing") {
-        d3.csv('datasets/lunch_dataset/math_stats.csv').then(makeMathChart);
+        d3.csv('datasets/lunch_dataset/writing_stats.csv').then(makeWritingChart);
     }
 }
