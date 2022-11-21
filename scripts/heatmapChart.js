e1bf432ee4f8daeb8756de375ae5e9f83cf3745c
Arthur Chin <77198012+AVCTY@users.noreply.github.com>
2022-11-21 16:14:07 +0000

    updated files

--- scripts/heatmapChart.js
+++ scripts/heatmapChart.js
@@ -5,115 +5,103 @@ function getPointCategoryName(point, dimension) {
     return axis.categories[point[isY ? 'y' : 'x']];
 }
 
-Highcharts.chart('heatmapContainer', {
-
-    chart: {
-        type: 'heatmap',
-        marginTop: 40,
-        marginBottom: 80,
-        plotBorderWidth: 1,
-        backgroundColor: '#EFEFEF',
-        height: 500,
-    },
 
 
-    title: {
-        text: '',
-    },
+$.get("datasets/studytimedata.csv", function (CSVdata) {
+    // CSVdata is populated with the file contents...
+    // ...ready to be converted into an Array
+    data = $.csv.toArrays(CSVdata);
+    //parsed csv array's value is converted to actual numbers instead of string
+    nums = data.map(arr => arr.map(value => Number(value)));
 
-    xAxis: {
-        categories: ['1hr Study Time', '2hrs Study Time', '3hrs Study Time', '4hrs Study Time']
-    },
+    Highcharts.chart('heatmapContainer', {
+        chart: {
+            type: 'heatmap',
+            marginTop: 40,
+            marginBottom: 80,
+            plotBorderWidth: 1,
+            backgroundColor: '#EFEFEF',
+            height: 700,
+        },
 
-    yAxis: {
-        categories: ['1hr Free Time', '2hrs Free Time', '3hrs Free Time', '4hrs Free Time', '5hrs Free Time'],
-        title: null,
-        reversed: true
-    },
 
-    accessibility: {
-        point: {
-            descriptionFormatter: function (point) {
-                var ix = point.index + 1,
-                    xName = getPointCategoryName(point, 'x'),
-                    yName = getPointCategoryName(point, 'y'),
-                    val = point.value;
-                return ix + '. ' + xName + ' sales ' + yName + ', ' + val + '.';
+        title: {
+            text: ''
+        },
+
+        xAxis: {
+            categories: ['1hr Study Time', '2hrs Study Time', '3hrs Study Time', '4hrs Study Time']
+        },
+
+        yAxis: {
+            categories: ['1hr Free Time', '2hrs Free Time', '3hrs Free Time', '4hrs Free Time', '5hrs Free Time'],
+            title: null,
+            reversed: true
+        },
+
+        accessibility: {
+            point: {
+                descriptionFormatter: function (point) {
+                    var ix = point.index + 1,
+                        xName = getPointCategoryName(point, 'x'),
+                        yName = getPointCategoryName(point, 'y'),
+                        val = point.value;
+                    return ix + '. ' + xName + ' sales ' + yName + ', ' + val + '.';
+                }
             }
-        }
-    },
+        },
 
-    colorAxis: {
-        min: 0,
-        minColor: '#E6F0FF',
-        maxColor: '#003380'
-    },
+        colorAxis: {
+            min: 0,
+            minColor: '#E6F0FF',
+            maxColor: '#003380'
+        },
 
-    legend: {
-        align: 'right',
-        layout: 'vertical',
-        margin: 0,
-        verticalAlign: 'top',
-        y: 25,
-        symbolHeight: 380
-    },
+        legend: {
+            align: 'right',
+            layout: 'vertical',
+            margin: 0,
+            verticalAlign: 'top',
+            y: 25,
+            symbolHeight: 580
+        },
 
-    tooltip: {
-        formatter: function () {
-            return '<b>' + getPointCategoryName(this.point, 'x') + '</b> for <br><b>' +
-                this.point.value + '</b> student/s with <br><b>' + getPointCategoryName(this.point, 'y') + '</b>';
-        }
-    },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + getPointCategoryName(this.point, 'x') + '</b> for <br><b>' +
+                    this.point.value + '</b> student/s with <br><b>' + getPointCategoryName(this.point, 'y') + '</b>';
+            }
+        },
 
-    series: [{
-        name: 'Sales per employee',
-        borderWidth: 1,
-        data: [
-            [0, 0, 2],
-            [0, 1, 19],
-            [0, 2, 35],
-            [0, 3, 29],
-            [0, 4, 20],
-            [1, 0, 11],
-            [1, 1, 28],
-            [1, 2, 82],
-            [1, 3, 61],
-            [1, 4, 16],
-            [2, 0, 3],
-            [2, 1, 11],
-            [2, 2, 29],
-            [2, 3, 21],
-            [2, 4, 1],
-            [3, 0, 3],
-            [3, 1, 6],
-            [3, 2, 11],
-            [3, 3, 4],
-            [3, 4, 3],
-        ],
-        dataLabels: {
-            enabled: true,
-            color: '#000000'
-        }
-    }],
+        series: [{
+            name: 'Sales per employee',
+            borderWidth: 1,
+            data: nums,
+            dataLabels: {
+                enabled: true,
+                color: '#000000'
+            }
+        }],
 
-    responsive: {
-        rules: [{
-            condition: {
-                maxWidth: 700
-            },
-            chartOptions: {
-                yAxis: {
-                    labels: {
-                        formatter: function () {
-                            return this.value.charAt(0);
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 900
+                },
+                chartOptions: {
+                    yAxis: {
+                        labels: {
+                            formatter: function () {
+                                return this.value.charAt(0);
+                            }
                         }
                     }
                 }
-            }
-        }]
-    },
+            }]
+        },
 
-    credits: {
-        enabled: false
-    }
+        credits: {
+            enabled: false
+        }
+    });
 });
