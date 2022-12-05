44bf7e401feb9600b34559f3c0d5e58583f1e886
Arthur Chin <77198012+AVCTY@users.noreply.github.com>
2022-12-05 11:36:01 +0000

    update files

---
+++ scripts/raisedHandsChart.js
@@ -0,0 +1,82 @@
+d3.csv('datasets/dataset3/raisedhandsdata.csv').then(raisedHandsChart);
+
+function raisedHandsChart(myData) {
+    var raisedHands = myData.map(function (d) {
+        return d.raisedHands;
+    });
+    var gradeL = myData.map(function (d) {
+        return +d.low;
+    });
+    var gradeM = myData.map(function (d) {
+        return +d.medium;
+    });
+    var gradeH = myData.map(function (d) {
+        return +d.high;
+    });
+
+    var myChart = new Chart("RHChart", {
+        type: 'bar',
+        data: {
+            labels: raisedHands,
+            datasets: [{
+                label: 'High Grade',
+                data: gradeH,
+                backgroundColor: '#59A14F',
+                borderWidth: 1.5,
+                barPercentage: 0.8,
+                categoryPercentage: 0.8,
+            }, {
+                label: 'Medium Grade',
+                data: gradeM,
+                backgroundColor: '#F28E2B',
+                borderWidth: 1.5,
+                barPercentage: 0.8,
+                categoryPercentage: 0.8,
+            }, {
+                label: 'Low Grade',
+                data: gradeL,
+                backgroundColor: '#E15759',
+                borderWidth: 1.5,
+                barPercentage: 0.8,
+                categoryPercentage: 0.8,
+            }],
+        },
+        options: {
+            tooltips: {
+                displayColors: true,
+                callbacks: {
+                    mode: 'x',
+                },
+            },
+            scales: {
+                x: {
+                    stacked: true,
+                    gridLines: {
+                        display: false,
+                    },
+                    ticks: {
+                        padding: 10,
+                    },
+                    title: {
+                        display: true,
+                        text: "Amount of times student raised hand",
+                    },
+                },
+                y: {
+                    stacked: true,
+                    ticks: {
+                        beginAtZero: true,
+                        padding: 10,
+                    },
+                    title: {
+                        display: true,
+                        text: "Student Percentage (%)",
+                    },
+                    type: 'linear',
+                },
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+        }
+    });
+}
